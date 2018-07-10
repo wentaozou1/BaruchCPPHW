7cbad0eec3645beecb7521556dbadae86813c41c
Wentao Zou <wz703@nyu.edu>
2018-07-10 09:40:59 +0800

    Update Defs.h

--- Fulin Li Level 2 HW Submission/1.6/1.6.1/1.6.1/Defs.h
+++ Fulin Li Level 2 HW Submission/1.6/1.6.1/1.6.1/Defs.h
@@ -6,9 +6,9 @@
 // @version 1.0 11/01/14
 
 #ifndef PRINT1
-#define PRINT1(a, b) printf("a = %lf\n", a)
+#define PRINT1(a, b) printf("a = %s\n", a)
 #endif 
 
 #ifndef PRINT2
-#define PRINT2(a, b) printf("a = %lf, b = %lf\n", a, b)
+#define PRINT2(a, b) printf("a = %s, b = %s", a, b)
 #endif 
